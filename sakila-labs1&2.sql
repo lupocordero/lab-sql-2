0937cc4969ffcaa0c55247d275112a44363baec0
LUPOCORDERO <lupo.cordero@gmail.com>
2021-02-02 22:10:51 +0100

    finished lab

--- sakila-labs1&2.sql
+++ sakila-labs1&2.sql
@@ -39,6 +39,41 @@ select title from sakila.film
 ORDER BY LENGTH(title) desc 
 LIMIT 1; -- ARACHNOPHOBIA ROLLERCOASTER
 
+-- How many distinct (different) actors' last names are there?
+select count(distinct last_name) from sakila.actor; -- 121
+
+-- In how many different languages where the films originally produced? (Use the column language_id from the film table)
+select count(distinct language_id) from sakila.film;
+
+-- How many movies were released with "PG-13" rating?
+select count(rating) from sakila.film
+where rating = "PG-13";
+
+-- Get 10 the longest movies from 2006.
+select title, length from sakila.film
+where release_year = '2006'
+limit 10;
+
+-- How many days has been the company operating (check DATEDIFF() function)?
+select datediff(max(rental_date), min(rental_date)) from sakila.rental; -- 266
+
+-- Show rental info with additional columns month and weekday. Get 20.
+select date_format(convert(rental_date,date),'%M') as 'Rental Month', date_format(convert(rental_date, datetime), '%W') as 'Weekday' from rental
+limit 20;
+
+-- Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
+select date_format(convert(rental_date,date),'%M') as 'Rental Month', date_format(convert(rental_date, datetime), '%W') as 'Weekday',
+case
+when date_format(convert(rental_date, datetime), '%W') = 'Saturday' or 'Sunday' then 'Weekend'
+else 'Workday'
+end as 'Day-Type'
+from rental
+
+-- How many rentals were in the last month of activity?
+select count(rental_id) from sakila.rental
+where date_format(convert(rental_date, date), '%M') = 'February' and date_format(convert(rental_date, date), '%Y') = '2006';
+
+
 
 
 
